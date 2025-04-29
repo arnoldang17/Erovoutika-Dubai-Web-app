dd32030810a9fa508f3b24f239f893437b652427
Arnold <aiy99673120@gmail.com>
2025-04-30 00:45:47 +0800

    fix home responsiveness

--- src/components/Home/Partners.tsx
+++ src/components/Home/Partners.tsx
@@ -8,12 +8,12 @@ export default function Partners() {
 return (
     <div className="flex flex-col items-center justify-center p-4 bg-gray-100">
       <h2 className="text-4xl font-bold mb-4">Our Partners</h2>
-        <Card className="w-fit bg-white shadow-md rounded-lg p-4">
+        <Card className="w-full bg-white shadow-md rounded-lg p-4">
             <CardContent className="flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center mb-4">
                 {/* <h3 className="text-xl font-semibold">We are proud to partner with:</h3> */}
             </div>
-            <div className="flex items-center justify-center p-1 gap-6">
+            <div className="flex items-center flex-wrap justify-center p-1 gap-6">
                 {images.map((image, index) => (
                 <div key={index} className="flex items-center w-[200px] h-[200px]">
                     <img
